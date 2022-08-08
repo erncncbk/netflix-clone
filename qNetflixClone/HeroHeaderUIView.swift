1f90beb0961d736ae205b9c8a6142a0bd139effd
Erencan <erncncbk@gmail.com>
2022-08-09 00:48:05 +0100

    Implementing buttons with constraint into the HeroHeader

--- qNetflixClone/HeroHeaderUIView.swift
+++ qNetflixClone/HeroHeaderUIView.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class HeroHeaderUIView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+
+        applyConstraints()
+    }
 
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
@@ -27,12 +37,47 @@ class HeroHeaderUIView: UIView {
         layer.addSublayer(gradientLayer)
     }
     
+    private let playButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+        
+    }()
+    private let downloadButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+        
+    }()
+    
+    private func applyConstraints() {
+        
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 50),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor,constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        let downloadButtonConstraints = [
+           downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -50),
+           downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor,constant: -50),
+           downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        addSubview(heroImageView)
-        addGradient()
     }
+    
+
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
