7970de8c643159da690fb77d7df256019db03865
Erencan <erncncbk@gmail.com>
2022-08-09 00:36:04 +0100

    Adding gradient for HeroHeader

--- qNetflixClone/HeroHeaderUIView.swift
+++ qNetflixClone/HeroHeaderUIView.swift
@@ -16,10 +16,22 @@ class HeroHeaderUIView: UIView {
         imageView.image = UIImage(named: "dune")
         return imageView
     }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
+        addGradient()
     }
     
     required init?(coder: NSCoder) {
