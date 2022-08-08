315add2e717b1888b0e6764986fe555949c0786f
Erencan <erncncbk@gmail.com>
2022-08-08 22:42:20 +0100

    Initial Commit

---
+++ qNetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qNetflixClone
+//
+//  Created by Erencan on 8.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
