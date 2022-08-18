4acf8ee915305d267b7592b603ebb48a223ebc5b
Erencan <erncncbk@gmail.com>
2022-08-18 23:31:04 +0100

    Adding tableview header

--- qNetflixClone/HomeViewController.swift
+++ qNetflixClone/HomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    let sectionTitles: [String] = ["Trending Movie","Popular","Trending TV","Upcoming Movies","Top Rated"]
 
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
@@ -53,7 +54,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController : UITableViewDelegate,UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        20
+        sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -70,6 +71,18 @@ extension HomeViewController : UITableViewDelegate,UITableViewDataSource {
   
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         200
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x+20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
